29597f89bd02562709976ffda6a6300f3b77129a
amir243-dev <amirajibola243@gmail.com>
2026-07-12 22:08:32 +0100

    add student model

---
+++ models/student.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  age: { type: Number, required: true },
+  // Enum restricts this field to only these exact values, preventing bad data entry.
+  gender: { type: String, required: true, enum: ["Male", "Female"] },
+  school: { type: String, required: true },
+  guardianName: { type: String, required: true },
+  guardianPhoneNumber: { type: String, rquired: true },
+  needs: [{ type: String }],
+  createdAt: { type: Date, default: Date.now() },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
