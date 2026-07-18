33744d118aa10a7b741d6c2d139a5d6ad68367ed
amir243-dev <amirajibola243@gmail.com>
2026-07-18 21:34:13 +0100

    add basic html form for donations

--- models/donation.model.js
+++ models/donation.model.js
@@ -5,7 +5,7 @@ const donationSchema = new mongoose.Schema({
   donorName: { type: String, required: true },
   // 'type' is a reserved word in some contexts, but fine as a key.
   // Using enum to restrict it to Cash or Kind (goods).
-  type: { type: String, required: true, enum: ["Cash", "Items"] },
+  type: { type: String, required: true, enum: ["Cash", "Kind"] },
   description: { type: String },
   amount: { type: Number, required: true },
   contact: { type: String, required: true },
